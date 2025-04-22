2d412a840c22b6c84b1488c17420829f0c0c2b7d
Quang Phan <vnphanquang@gmail.com>
2025-04-22 09:37:57 +0700

    feat: add a /construction page

---
+++ src/routes/construction/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import svgConstruction from '$lib/assets/images/under-construction.svg?url';
+</script>
+
+<main class="grid place-items-center h-screen p-10">
+	<img
+		alt="Under construction"
+		class="w-120 max-w-full h-auto"
+		src={svgConstruction}
+		fetchpriority="high"
+		loading="eager"
+		decoding="sync"
+	/>
+</main>
